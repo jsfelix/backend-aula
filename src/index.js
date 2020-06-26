b0f2b483b95dac452c355e2bb63bb5a4ffbbf8b4
Jefferson Felix <jsfelix@gmail.com>
2020-06-26 19:48:26 -0300

    Cria os primeiros recursos da API

---
+++ src/index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (request, response) => {
+  return response.json({ message:'Hello Jeff!'});
+});
+
+app.get('/projects', (request, response) => {
+  return response.json([
+    'Projeto 1',
+    'Projeto 2',
+  ]);
+});
+
+app.post('/projects', (request, response) => {
+  return response.json([
+    'Projeto 1',
+    'Projeto 2',
+    'Projeto 3',
+  ]);
+});
+
+app.put('/projects/:id', (request, response) => {
+  return response.json([
+    'Projeto 4',
+  ]);
+});
+
+app.delete('/projects/:id', (request, response) => {
+  return response.json([
+    'Projeto 2',
+    'Projeto 3',
+  ]);
+});
+
+app.listen(3333, () => {
+  console.log('🚀 Backend started!')
+});
